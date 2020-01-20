6fb283eec1a694c133a640c990c2923518f81366
Yuhao Chen <57876857+AkazaAkane@users.noreply.github.com>
2020-01-20 16:51:21 -0500

    Add files via upload

--- practice/PCC chapter9/restaurant.py
+++ practice/PCC chapter9/restaurant.py
@@ -3,6 +3,7 @@ class Restaurant():
     def __init__(self,restaurant_name,cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print("Restaurant name is {}".format(self.restaurant_name))
@@ -11,6 +12,25 @@ class Restaurant():
     def open_restaurant(self):
         print("Restaurant is on")
 
+    def set_number_served(self,num):
+        if self.number_served >= 0:
+            self.number_served = num
+        else:
+            print("You can not serve negative people")
+
+    def increment_number_served(self,num):
+        self.number_served += num
+
+    def read_number_served(self):
+        print("There are {} people".format(self.number_served))
+
+
 mcd = Restaurant("Mc Donald","Hamburger, fries")
 mcd.describe_restaurant()
 mcd.open_restaurant()
+
+mcd.read_number_served()
+mcd.set_number_served(100)
+mcd.read_number_served()
+mcd.increment_number_served(10)
+mcd.read_number_served()
