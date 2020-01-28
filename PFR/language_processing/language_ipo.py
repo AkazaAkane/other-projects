bd94490d59443790c6c4f2c6020e8d2296843836
Yuhao Chen <57876857+AkazaAkane@users.noreply.github.com>
2020-01-27 22:00:47 -0500

    Add files via upload

--- PFR/language_processing/language_ipo.py
+++ PFR/language_processing/language_ipo.py
@@ -19,3 +19,18 @@ def count_words(text):
     return word_counts
 
 print(count_words(text))
+
+def read_book(title_path):
+
+    with open(title_path, "r", encoding = "uft8") as current_file:
+        text = current_file.read()
+        text = text.replace("\n",".").replace("\r","")
+    return text
+
+def word_stats(word_counts):
+    """
+    return number of unique words and word frequencies
+    """
+    num_unique = len(word_counts)
+    counts = word_counts.values()
+    return (num_unique,counts)
