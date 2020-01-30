93144fae3e7c9d1dbbd79e19283e1245d4b6fbf9
Yuhao Chen <57876857+AkazaAkane@users.noreply.github.com>
2020-01-29 22:12:29 -0500

    Add files via upload

--- PFR/language_processing/language_ipo.py
+++ PFR/language_processing/language_ipo.py
@@ -1,36 +1,36 @@
 #from collections import Counter
-text = "jia you, ao li gei"
+class Language_ipo():
+
+    def __init__(self,text,title_path,word_counts)
+
+    def count_words(text):
+
+        text = text.lower()
+        skips = [".",".",";",":","'",'"']
+        for ch in skips:
+            text = text.replace(ch,"")
+
+        #word_counts = Counter(text.split(""))
+        word_counts = {}
+        for word in text.split(" "):
+            if word in word_counts:
+                word_counts[word] += 1
+            else:
+                word_counts[word] = 1
+        return word_counts
 
 
-def count_words(text):
+    def read_book(title_path):
 
-    text = text.lower()
-    skips = [".",".",";",":","'",'"']
-    for ch in skips:
-        text = text.replace(ch,"")
+        with open(title_path, "r", encoding = "uft8") as current_file:
+            text = current_file.read()
+            text = text.replace("\n",".").replace("\r","")
+        return text
 
-    #word_counts = Counter(text.split(""))
-    word_counts = {}
-    for word in text.split(" "):
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
-    return word_counts
-
-print(count_words(text))
-
-def read_book(title_path):
-
-    with open(title_path, "r", encoding = "uft8") as current_file:
-        text = current_file.read()
-        text = text.replace("\n",".").replace("\r","")
-    return text
-
-def word_stats(word_counts):
-    """
-    return number of unique words and word frequencies
-    """
-    num_unique = len(word_counts)
-    counts = word_counts.values()
-    return (num_unique,counts)
+    def word_stats(word_counts):
+        """
+        return number of unique words and word frequencies
+        """
+        num_unique = len(word_counts)
+        counts = word_counts.values()
+        return (num_unique,counts)
