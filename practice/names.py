902ae0878c38a3281f71ae2d2ec29178a29e4730
Yuhao Chen <57876857+AkazaAkane@users.noreply.github.com>
2020-02-02 14:01:50 -0500

    Add files via upload

---
+++ practice/names.py
@@ -0,0 +1,8 @@
+apt201_names = ['August','Franklin','Charlie']
+for i in apt201_names:
+    print(i)
+    print("Hi, " + i + " !")
+
+commute = ['bus','motor','bicycle']
+for i in commute:
+    print()
