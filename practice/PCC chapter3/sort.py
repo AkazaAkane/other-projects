d6d531fb0b7b51d3ade5ef11521751854c30f67f
Yuhao Chen <57876857+AkazaAkane@users.noreply.github.com>
2020-02-02 14:18:34 -0500

    Add files via upload

---
+++ practice/PCC chapter3/sort.py
@@ -0,0 +1,12 @@
+travel = ['Japan','Tokyo','Kyoto','Marutai','Hokkaido']
+print(travel)
+print(sorted(travel))
+print(travel)
+print(sorted(travel,reverse = True))
+print(travel)
+travel.reverse()
+print(travel)
+travel.reverse()
+print(travel)
+travel.sort()
+print(travel)
