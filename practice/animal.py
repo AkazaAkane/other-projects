923a824538fb4aeb71a7283f62452637d7ae9f73
Yuhao Chen <57876857+AkazaAkane@users.noreply.github.com>
2020-02-03 23:04:34 -0500

    Add files via upload

---
+++ practice/animal.py
@@ -0,0 +1,6 @@
+pets = ["dog","cat","giao"]
+
+for pet in pets:
+    print("A {} would make a great pet".format(pet))
+
+print("Any of these animals would make a great pet!")
---
+++ practice/pizza.py
@@ -0,0 +1,5 @@
+pizza = ['baffulo','kaizen','super']
+for i in pizza:
+    print(i)
+    print("I really like " + i + " pizza!")
+print("I really love pizza!")
